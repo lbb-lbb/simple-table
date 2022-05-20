3b9cd12c870e064735ff800c2962f42941471794
李学彦 <llxxyy_yy@163.com>
2022-05-20 17:40:58 +0800

    一处vuepress

--- doc/.vuepress/clientAppEnhance.js
+++
@@ -1,9 +0,0 @@
-import Antd from 'ant-design-vue'
-import 'ant-design-vue/dist/antd.css'
-
-
-import { defineClientAppEnhance } from '@vuepress/client'
-
-export default defineClientAppEnhance(({ app, router, siteData }) => {
-  app.use(Antd)
-})
